80fd31d1883fd95154a666af73efa28cdacc3237
Romanoffy <namanyaxalf@gmail.com>
2025-01-22 11:44:34 +0700

    product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -14,10 +15,14 @@ class ProductController extends Controller
     public function index()
     {
         $data = Product::all();
-        return response([
+        return $data;
+        return $data->count() > 0 ? response([
             'message' => 'Product has been founded',
             'data' => $data
-        ]);
+        ], 200) : response([
+            'message' => 'Product has not found',
+            'data' => $data
+        ], 404);
     }
 
     /**
@@ -27,11 +32,15 @@ class ProductController extends Controller
     {
         $request->validate([
             'product_name' => 'required|string',
-            'photo' => 'required|image:jpg,jpeg|max:1024',
+            'photo' => 'required|image:jpg,jpeg,png|max:1024',
             'category_id' => 'required|exists:categories,id',
             'price' => 'required|digits_between:5,10',
-            'qty' => 'required|integer'
+            'qty' => 'required|integer',
+            'description' => 'required|string'
         ]);
+
+
+
 
         $time = Carbon::now()->format("Y-m-d_H_i_s");
         $photo = $time . '.' . $request->photo->extension();
@@ -44,7 +53,8 @@ class ProductController extends Controller
             'photo_name' => $photo,
             'category_id' => $request->category_id,
             'price' => $request->price,
-            'qty' => $request->qty
+            'qty' => $request->qty,
+            'description' => $request->description
         ]);
 
         return response([
@@ -55,24 +65,79 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show( $product)
     {
-        //
+        $data = Product::find($product);
+        return isset($data) ? response([
+            'message' => 'Product detail has been founded',
+            'data' => $data
+        ], 200) : response([
+            'message' => 'Product detail has not found',
+            'data' => $data
+        ], 404);
+        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $product)
     {
-        //
+       
+        $request->validate([
+            'product_name' => 'required|string',
+            'category_id' => 'required|exists:categories,id',
+            'price' => 'required|digits_between:5,10',
+            'qty' => 'required|integer',
+            'description' => 'required|string'
+        ]);
+
+        $data = Product::find($product);
+
+        if(!isset($data)){
+            return response([
+                'message' => 'Product Not Found'
+            ],404);
+        }
+
+        if(isset($request->photo)){
+            $request->validate([
+                "photo" => 'required|image:jpg,jpeg,png|max:1024',
+            ]);
+        }
+
+      
+            $data->product_name = $request->product_name;
+            $data->category_id = $request->category_id;
+            $data->price = $request->price;
+            $data->qty = $request->qty;
+            $data->description = $request->description;
+            $data->save();
+
+            return response([
+                'message' => 'Product has been updated'
+            ],200);
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy ($product)
     {
-        //
+        $data = Product::find($product);
+     
+        if(!isset($data)){
+            return response([
+                'message' => 'Product Not Found'
+            ],404);
+        }
+
+        File::delete(public_path("upload/product") . "/" . $data->product_image_name);
+        $data->delete();
+
+        return response([
+            'message' => 'Product has been deleted !'
+        ],200);
     }
 }
